925b83b8c899563d58b9c1dafe039cef16dc5eb0
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2022-05-05 16:05:02 +0100

    Start doing Program

--- AnimInterf/Program.cs
+++ AnimInterf/Program.cs
@@ -4,9 +4,51 @@ namespace AnimInterf
 {
     class Program
     {
-        static void Main(string[] args)
+        Animal[] Animal_Array = new Animal[10];
+        static async void Main(string[] args)
         {
-            
+            int num;
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                num = rnd.Next(0, 4);
+                switch(num)
+                {
+                    case 0:
+                        Animal_Array[i] = new Bat();
+                        break;
+                    case 1:
+                        Animal_Array[i] = new Bee();
+                        break;
+                    case 2:
+                        Animal_Array[i] = new Cat();
+                        break;
+                    case 3:
+                        Animal_Array[i] = new Dog();
+                        break;
+                }
+            }
+
+            DoTheThing(Animal_Array);
+
+        }
+    }
+
+    public void DoTheThing(Animal_Array)
+    {
+        foreach(Animal animal in Animal_Array)
+        {
+            Console.WriteLine(animal.Sound());
+            if (animal is IMammal)
+            {
+                Console.WriteLine(animal.NumberOfNipples());
+            }
+            if (animal is IFly)
+            {
+                Console.WriteLine(animal.NumberOfWings());
+            }
         }
     }
 }
